ea7ce629d8581aec4a67bb5600e411a03aa051a4
RNAlexander <rnalexander@gmail.com>
2012-05-31 16:51:36 +0100

    Ryan / Graham - Got the football tests passing

--- src/main/scala/FootballMunging.scala
+++ src/main/scala/FootballMunging.scala
@@ -1,5 +1,29 @@
 package org.timgroup.dojo
 
 case class FootballMunging(val lines: List[String]) {
-  def teamWithTheSmallestDifferenceBetweenForAndAgainst = ""
+  import FootballMunging._
+
+  def teamWithTheSmallestDifferenceBetweenForAndAgainst =
+    team_with_min_goal_difference(team_lines_from(lines).map(team_from_team_line)).name
 }
+
+case class Team(val name: String, val goalsFor: Int, val goalsAgainst: Int) {
+  def difference() = Math.abs(goalsFor - goalsAgainst)
+}
+
+
+object FootballMunging {
+  def team_lines_from(lines: List[String]) = lines filter starts_with_number
+
+  def team_from_team_line(line: String) = {
+    val words = line split """\s+"""
+    Team(words(2), words(7).toInt, words(9).toInt)
+  }
+  
+  def starts_with_number(line: String) = line matches """\s+\d+.*"""
+
+  def team_with_min_goal_difference(teams: List[Team]) = {
+    teams.sort((l, r) => l.difference < r.difference)(0)
+  }
+}
+
--- src/test/scala/org/timgroup/dojo/FootballMungingTest.scala
+++ src/test/scala/org/timgroup/dojo/FootballMungingTest.scala
@@ -11,4 +11,21 @@ class FootballMungingTest extends FunSuite with ShouldMatchers with ReadFile {
 
     teamWithTheSmallestDifferenceBetweenForAndAgainst should be("Aston_Villa")
   }
+
+  test("Read the first team line from the football lines") {
+    FootballMunging.team_lines_from(List("Source", "    1. Wimbledon AFC")) should be (List("    1. Wimbledon AFC"))
+  } 
+
+  test("Converts a team line to a team object") {
+    FootballMunging.team_from_team_line("    5. Leeds           38    18  12   8    53  -  37    66") should be(Team("Leeds", 53, 37))
+  }  
+  
+  test("Find team with smallest goal difference") {
+    val team1 = Team("Foo1", 10, 11)    
+    val team2 = Team("Foo2", 50, 0)
+    val team3 = Team("Foo3", 0, 56)
+    FootballMunging.team_with_min_goal_difference(List(team1, team2, team3)) should be(team1)
+  }
+  
 }
+
